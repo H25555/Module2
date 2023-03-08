54340a41d0aa81fabef28f2a9a4587cedd370c84
H25555 <counterbigdenchet@gmail.com>
2023-03-08 16:20:23 +0700

    add .

---
+++ demo1/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 30;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Huế";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
---
+++ helloworld/src/main/java/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 1;
+        int b = 2;
+        int c = a++;
+        System.out.println(c);
+        System.out.println(a);
+    }
+}
